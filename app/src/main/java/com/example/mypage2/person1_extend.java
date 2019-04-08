c46c75ed6139a3bf50989f26dfe2c1896df59404
nturk <nart_turk_tp@bmc.com>
2019-04-09 00:12:43 +0300

    person1(nart)

---
+++ app/src/main/java/com/example/mypage2/person1_extend.java
@@ -0,0 +1,13 @@
+package com.example.mypage2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class person1_extend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person1_extend);
+    }
+}
