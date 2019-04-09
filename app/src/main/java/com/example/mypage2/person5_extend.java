e251454e01e3896790833a610978aa28f82c2c94
Nati_Shchiglik <natishch94@gmail.com>
2019-04-09 10:24:11 +0300

    person5(nati)

---
+++ app/src/main/java/com/example/mypage2/person5_extend.java
@@ -0,0 +1,30 @@
+package com.example.mypage2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class person5_extend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person5_extend);
+
+        int likesCnt;
+        int followersCnt;
+        TextView likes;
+        TextView followers;
+
+        Intent intent = getIntent();
+        likesCnt = intent.getIntExtra("likesCnt",0);
+        followersCnt = intent.getIntExtra("followersCnt",0);
+
+        likes = findViewById(R.id.likesCntE);
+        followers = findViewById(R.id.followersCntE);
+
+        likes.setText(String.valueOf(likesCnt));
+        followers.setText(String.valueOf(followersCnt));
+    }
+}
